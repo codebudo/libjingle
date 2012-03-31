e20628afdaf6e099546894ed1426f2bacfe6247a
S. Mikkel Wilson <sebastian.wilson@kace.com>
2012-03-31 16:17:20 -0700

    add dependency submodules and Makefiles to build these deps.

--- get_third_party.sh
+++ get_third_party.sh
@@ -1,43 +1,13 @@
 #!/bin/sh
 
-if [ ! -e talk/third_party/expat-2.0.1 ]; then
-	if [ ! -e expat-2.0.1.tar.gz ]; then
-		echo "Downloading expat..."
-                wget http://sourceforge.net/projects/expat/files/expat/2.0.1/expat-2.0.1.tar.gz/download -O expat-2.0.1.tar.gz
-	fi
-	echo -n "Extracting expact 2.0.1..."
-	tar zxpf expat-2.0.1.tar.gz -C talk/third_party
-	echo "done"
-fi
+#git submodule init
 
-if [ ! -e talk/third_party/srtp ]; then
+if [ ! -e vendor/srtp ]; then
 	echo -n "Getting latest srtp..."
-	cd talk/third_party
+    pushd .
+	cd vendor
 	cvs -d:pserver:anonymous@srtp.cvs.sourceforge.net:/cvsroot/srtp co -P srtp
 	echo "done"
-	cd ../../
+    popd
 fi
 
-if [ ! -e talk/third_party/gtest ]; then
-  if [ ! -e gtest-1.6.0.zip ]; then
-    echo "Downloading gtest..."
-    wget http://googletest.googlecode.com/files/gtest-1.6.0.zip -O gtest-1.6.0.zip
-  fi
-
-  echo -n "Extracting gtest..."
-  cd talk/third_party
-  unzip -x ../../gtest-1.6.0.zip
-  mv gtest-1.6.0 gtest
-  echo "done"
-  cd ../../
-fi
-
-if [ ! -e talk/third_party/jsoncpp-src-0.5.0 ]; then
-	if [ ! -e jsoncpp-src-0.5.0.tar.gz ]; then
-		echo "Downloading jsoncpp..."
-                wget http://sourceforge.net/projects/jsoncpp/files/latest/download -O jsoncpp-src-0.5.0.tar.gz
-	fi
-	echo -n "Extracting jsoncpp 0.5.0..."
-	tar zxpf jsoncpp-src-0.5.0.tar.gz -C talk/third_party
-	echo "done"
-fi
